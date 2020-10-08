aa737ca334beeb9169bddc0bd0c6641003559f8f
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-10-08 15:20:25 +0200

    built the frontend

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
-	// ScrollMagic code will go here
+
+	// Init ScrollMagic Controller
+	const controller = new ScrollMagic.Controller();
+
+	// Scene 1 - pin our main section
+	const pinScene01 = new ScrollMagic.Scene({
+		triggerElement: '#main',
+		triggerHook: 0,
+		duration: '900%'
+	})
+	.setPin("#main .pin-wrapper", {pushFollowers: true})
+	.addTo(controller);
 
 });
